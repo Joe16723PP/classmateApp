2b046f1bc43e374cb82c217f2102b2fe6ba4d16f
Jirot Chaitamart <jirot_c@kkumail.com>
2019-02-20 17:11:20 +0700

    firstProject

---
+++ extraProject/app/src/main/java/kku/en/coe/extraproject/SecondActivity.java
@@ -0,0 +1,17 @@
+package kku.en.coe.extraproject;
+
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
