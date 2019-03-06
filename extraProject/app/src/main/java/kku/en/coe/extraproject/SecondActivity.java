2b0623722888e231a86da5fc4e3e8ae76bb2d741
teerapong <teerapong.w@kkumail.com>
2019-03-06 16:59:47 +0700

    Oat

--- extraProject/app/src/main/java/kku/en/coe/extraproject/SecondActivity.java
+++ extraProject/app/src/main/java/kku/en/coe/extraproject/SecondActivity.java
@@ -2,18 +2,36 @@ package kku.en.coe.extraproject;
 
 import android.app.AppComponentFactory;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
 import android.widget.EditText;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         setIntentData();
+
+
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com"));
+        if (intent != null) {
+            startActivity(intent);
+        }
+
     }
     public void setIntentData() {
         Bundle extras = getIntent().getExtras();
