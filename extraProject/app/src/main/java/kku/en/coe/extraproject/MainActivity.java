654feff72c96b02698c89f35184622fda0e2969d
Jirot Chaitamart <jirot_c@kkumail.com>
2019-03-25 18:49:27 +0700

    new structure : joe

--- extraProject/app/src/main/java/kku/en/coe/extraproject/MainActivity.java
+++ extraProject/app/src/main/java/kku/en/coe/extraproject/MainActivity.java
@@ -3,45 +3,18 @@ package kku.en.coe.extraproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button LoginBtn;
-    EditText UserName;
-    private static final int REQUEST_CODE = 10;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoginBtn = findViewById(R.id.loginBtn);
-        LoginBtn.setOnClickListener(this);
-        UserName = findViewById(R.id.UserName);
     }
 
-
-    @Override
-    public void onClick(View view) {
-        if (view == LoginBtn) {
-            Intent intent = new Intent(this, SecondActivity.class);
-            String getUserName = UserName.getText().toString();
-            intent.putExtra("userName", getUserName);
-            startActivityForResult(intent, REQUEST_CODE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-            String rtv =  data.getStringExtra("rtv");
-            TextView txtview = (TextView) findViewById(R.id.appName);
-            txtview.setText("User : " + rtv);
-            Log.d("chk", "onActivityResult: " + rtv);
-        }
-    }
 }
--- extraProject/app/src/main/java/kku/en/coe/extraproject/SecondActivity.java
+++ extraProject/app/src/main/java/kku/en/coe/extraproject/SecondActivity.java
@@ -2,53 +2,16 @@ package kku.en.coe.extraproject;
 
 import android.app.AppComponentFactory;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
-import android.widget.EditText;
-
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-    Spinner spinner;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        setIntentData();
-
-
-    }
-    public void onClick(View view) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com"));
-        if (intent != null) {
-            startActivity(intent);
-        }
-
-    }
-    public void setIntentData() {
-        Bundle extras = getIntent().getExtras();
-        String inputString = extras.getString("userName");
-        TextView view = (TextView) findViewById(R.id.userName);
-        view.setText(inputString);
     }
 
-    @Override
-    public void finish() {
-        Intent intent = new Intent();
-        TextView returnVal = findViewById (R.id.userName);
-        String rtv = returnVal.getText().toString();
-        intent.putExtra("rtv", rtv);
-        // return the Intent to the application
-        setResult(RESULT_OK, intent);
-        super.finish();
-
-    }
 }
