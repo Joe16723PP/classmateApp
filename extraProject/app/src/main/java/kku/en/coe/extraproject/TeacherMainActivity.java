4821ce732df0a2360b0457c58a67fbed9b0d924c
Jirot Chaitamart <jirot_c@kkumail.com>
2019-05-01 03:16:20 +0700

    bye

--- extraProject/app/src/main/java/kku/en/coe/extraproject/TeacherMainActivity.java
+++ extraProject/app/src/main/java/kku/en/coe/extraproject/TeacherMainActivity.java
@@ -1,9 +1,22 @@
 package kku.en.coe.extraproject;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,21 +27,35 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class TeacherMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
     FloatingActionButton fab;
     TextView user_name , user_email;
     NavigationView navigationView;
+    private FirebaseAuth mAuth;
+    FirebaseUser cur_user;
+    ImageView profile_pic;
+    LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        layout = findViewById(R.id.layout_cardView);
+        mAuth = FirebaseAuth.getInstance();
         setSupportActionBar(toolbar);
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(this);
@@ -41,20 +68,115 @@ public class TeacherMainActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
         getInitUser();
+//        createCardView();
+////        for (int i = 0; i < 2; i++) {
+////            createCardView();
+////        }
     }
+    private void createCardView(){
+        CardView cardview = new CardView(this);
+        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        cardview.setLayoutParams(p);
+//        cardView.setRadius(12);
+//        cardView.setMaxCardElevation(30);
+//        cardView.setMaxCardElevation(6);
+//        cardView.setBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimary));
+//        layout.addView(cardView , p);
+        cardview.setRadius(15);
 
+//        cardview.setPadding(25, 25, 25, 25);
+
+//        cardview.setCardBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimary));
+
+        cardview.setMaxCardElevation(30);
+
+        cardview.setMaxCardElevation(6);
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setWeightSum(3);
+
+        TextView textView = new TextView(this);
+        textView.setBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimary));
+        textView.setLayoutParams(p);
+
+        LinearLayout sub_linear = new LinearLayout(this);
+        sub_linear.setLayoutParams(p);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            sub_linear.setElevation(2);
+        }
+        sub_linear.setPadding(10,10,10,10);
+
+        TextView tv2 = new TextView(this);
+        tv2.setLayoutParams(p);
+        tv2.setText("CardView Programmatically");
+        tv2.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25);
+        tv2.setTextColor(Color.WHITE);
+        tv2.setPadding(25,25,25,25);
+        tv2.setGravity(Gravity.CENTER);
+
+        sub_linear.addView(tv2);
+        linearLayout.addView(textView);
+        linearLayout.addView(sub_linear);
+        cardview.addView(linearLayout);
+
+
+//        TextView textview = new TextView(this);
+//
+//        textview.setLayoutParams(p);
+//
+//        textview.setText("CardView Programmatically");
+//
+//        textview.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25);
+//
+//        textview.setTextColor(Color.WHITE);
+//
+//        textview.setPadding(25,25,25,25);
+//
+//        textview.setGravity(Gravity.CENTER);
+//
+//        cardview.addView(textview);
+        layout.addView(cardview);
+
+    }
     private void getInitUser() {
         Bundle extras = getIntent().getExtras();
         String name = extras.getString("name");
         String email = extras.getString("email");
+        cur_user = mAuth.getCurrentUser();
         View header = navigationView.getHeaderView(0);
         user_name = header.findViewById(R.id.user_name);
         user_email = header.findViewById(R.id.user_email);
+        profile_pic = header.findViewById(R.id.imageView);
+        Uri selectedImg = cur_user.getPhotoUrl();
+        new DownLoadImageTask(profile_pic).execute(String.valueOf(selectedImg));
         user_name.setText(name);
         user_email.setText(email);
+    }
+    private class DownLoadImageTask extends AsyncTask<String,Void,Bitmap> {
+        ImageView imageView;
 
-//        user_email.setText("email joe");
-//        user_name.setText("name joe");
+        public DownLoadImageTask(ImageView imageView){
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String...urls){
+            String urlOfImage = urls[0];
+            Bitmap logo = null;
+            try{
+                InputStream is = new URL(urlOfImage).openStream();
+                logo = BitmapFactory.decodeStream(is);
+            }catch(Exception e){ // Catch the download exception
+                e.printStackTrace();
+            }
+            return logo;
+        }
+
+        protected void onPostExecute(Bitmap result){
+            imageView.setImageBitmap(result);
+        }
     }
 
     @Override
@@ -122,7 +244,7 @@ public class TeacherMainActivity extends AppCompatActivity
     @Override
     public void onClick(View v) {
         if (v == fab) {
-            Intent intent = new Intent(this, CreateClassActivity.class);
+            Intent intent = new Intent(this, MapsActivity.class);
             startActivity(intent);
         }
     }
